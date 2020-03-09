c506954b3a2cba9aec02e73a9c3d738c6ebc5928
Anugrah Lambogo <lambogoai@gmail.com>
2020-03-09 01:03:32 -0500

    fixed comma in config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL: process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/noteful"
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'localhost:3000',
 };
