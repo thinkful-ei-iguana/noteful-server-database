c94febfe1e7b5843ecba13dc8a607684c1edfb25
Anugrah Lambogo <lambogoai@gmail.com>
2020-03-09 10:32:54 -0500

    changed config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'localhost:3000',
+  CLIENT_ORIGIN: 'https://noteful-app-ruddy.now.sh/' || 'localhost:3000',
 };
