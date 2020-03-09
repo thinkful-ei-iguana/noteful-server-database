d12489efd8a960113e0a3d852d4c23312cf58e2c
Anugrah Lambogo <lambogoai@gmail.com>
2020-03-09 01:14:35 -0500

    headers

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ app.use(cors({
 app.use(morgan(NODE_ENV === 'production' ? 'tiny' : 'common'));
 app.use(helmet());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://noteful-app-8xhsvl3e5.now.sh"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.use('/api/folders', foldersRouter);
 app.use('/api/notes', notesRouter);
